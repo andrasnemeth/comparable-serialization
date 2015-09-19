285d4f2ce73671d7b7d02f14c6eab0ce954f2379
András Németh <andrasnemeth@users.noreply.github.com>
2015-09-19 15:24:44 +0200

    Add mpl sequence check

--- include/serialization/Serial.hpp
+++ include/serialization/Serial.hpp
@@ -9,6 +9,9 @@
 #include <boost/concept/assert.hpp>
 #include <boost/mpl/contains.hpp>
 #include <boost/mpl/distance.hpp>
+#include <boost/mpl/deque.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/set.hpp>
 #include <boost/mpl/size.hpp>
 #include <boost/mpl/vector.hpp>
 #include <boost/operators.hpp>
@@ -51,23 +54,46 @@ struct ElementIndex {
 };
 
 //----------------------------------------------------------------------------//
+
+template<typename T>
+struct IsMplSequence : std::false_type {
+};
+
+template<typename ...Ts>
+struct IsMplSequence<boost::mpl::vector<Ts...>> : std::true_type {
+};
+
+template<typename ...Ts>
+struct IsMplSequence<boost::mpl::set<Ts...>> : std::true_type {
+};
+
+template<typename ...Ts>
+struct IsMplSequence<boost::mpl::list<Ts...>> : std::true_type {
+};
+
+template<typename ...Ts>
+struct IsMplSequence<boost::mpl::deque<Ts...>> : std::true_type {
+};
+
+//----------------------------------------------------------------------------//
 } // namespace detail
 //============================================================================//
 
 // In order to compare arbitrary types in serial form, we need to prepend all
 // raw data in a serial with type tags.
-template<typename ...SerializableDataPack>
-class Serial : boost::totally_ordered<Serial<SerializableDataPack...>> {
+template<typename SerializableData = boost::mpl::vector<>>
+class Serial : boost::totally_ordered<Serial<SerializableData>> {
 private:
-    using SerializableData = boost::mpl::vector<SerializableDataPack...>;
-
-    constexpr static const char CUSTOM_TYPE_OFFSET =
-            boost::mpl::size<detail::PackableData>::value;
+    static_assert(detail::IsMplSequence<SerializableData>::value,
+            "Template argument must be an mpl sequence!");
 
     static_assert(boost::mpl::size<SerializableData>::value
             + boost::mpl::size<detail::PackableData>::value
             < std::numeric_limits<std::int8_t>::max(),
             "Too many custom types!");
+
+    constexpr static const char CUSTOM_TYPE_OFFSET =
+            boost::mpl::size<detail::PackableData>::value;
 
 public:
     Serial() : readOffset(0) {
